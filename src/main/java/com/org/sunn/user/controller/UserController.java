3a2ad2d7891a3f25aa3a0fda0f4cff34cc4599a6
sunil <sunn.tripathi@gmail.com>
2022-01-07 23:01:44 +0530

    Added Logger

--- src/main/java/com/org/sunn/user/controller/UserController.java
+++ src/main/java/com/org/sunn/user/controller/UserController.java
@@ -2,10 +2,13 @@ package com.org.sunn.user.controller;
 
 import com.org.sunn.user.entity.User;
 import com.org.sunn.user.service.UserService;
+import com.sun.media.jfxmedia.logging.Logger;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Slf4j
 @RequestMapping("/users")
 public class UserController {
     @Autowired
@@ -13,11 +16,13 @@ public class UserController {
 
     @GetMapping("/{id}")
     public User getUser(@PathVariable("id") Long userId){
+        log.info("Get User Details inside UserController");
         return userService.getUserById(userId);
 
     }
     @PostMapping("/")
     public User saveUser(@RequestBody User user){
+        log.info("Save User Details inside UserController");
         return  userService.saveUser(user);
 
     }
