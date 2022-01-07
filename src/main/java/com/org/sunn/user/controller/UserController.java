41e0dd67619c518077e78d220a1cf30d2b08bf07
sunil <sunn.tripathi@gmail.com>
2022-01-07 23:26:43 +0530

    Fix SLF4J Logger

--- src/main/java/com/org/sunn/user/controller/UserController.java
+++ src/main/java/com/org/sunn/user/controller/UserController.java
@@ -2,11 +2,12 @@ package com.org.sunn.user.controller;
 
 import com.org.sunn.user.entity.User;
 import com.org.sunn.user.service.UserService;
-import com.sun.media.jfxmedia.logging.Logger;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Slf4j
 @RequestMapping("/users")
 public class UserController {
     @Autowired
@@ -14,11 +15,13 @@ public class UserController {
 
     @GetMapping("/{id}")
     public User getUser(@PathVariable("id") Long userId){
+        log.info("Get User Details inside UserController");
         return userService.getUserById(userId);
 
     }
     @PostMapping("/")
     public User saveUser(@RequestBody User user){
+        log.info("Save User Details inside UserController");
         return  userService.saveUser(user);
 
     }
